25831653febd2a8fab2ee6ad4fb56245aecfa8c8
IgnacioOE <141673811+IgnacioOE@users.noreply.github.com>
2024-04-09 00:30:41 +0000

    final

---
+++ assignments/00-ShapeCircleRectanglePolygon/Polygon.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "Shape.hpp"
+
+class Polygon : public Shape
+{
+    public:
+        Polygon();
+        Polygon(int valX, int valY, int valSides);
+        int getSides();
+    private:
+        int sides;
+};
